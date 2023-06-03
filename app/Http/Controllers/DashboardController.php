efcd9ca402344da8a8a5772c429d6f1745577394
Daniel Felix <felixdaniel2056@gmail.com>
2023-06-03 15:52:45 +0100

    chore: intervention image fix 3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -41,52 +41,43 @@ class DashboardController extends Controller
             if ($request->hasFile('site_header_logo')) {
                 $file = $request->file('site_header_logo');
 
-                // convert to svg
-                $manager = new ImageManager();
-                $image = $manager->make($file)->encode('png');
-                $file_name = 'logo.' . $image->extension();
-
                 // delete old logo
                 if ($site_settings->site_header_logo) {
-                    unlink(storage_path('app/public/logo/' . $file_name));
+                    unlink(storage_path('app/public/logo/' . $site_settings->site_header_logo));
                 }
 
-                $image->save(storage_path('app/public/logo/' . $file_name));
-                $site_settings->site_header_logo = config('app.url') . '/storage/logo/' . $file_name;
+                $file_name = 'site_header_logo.' . $file->getClientOriginalExtension();
+                $file->storeAs('public/logo', $file_name);
+
+                $site_settings->site_header_logo = $file_name;
             }
 
             if ($request->hasFile('site_footer_logo')) {
                 $file = $request->file('site_footer_logo');
 
-                // convert to svg
-                $manager = new ImageManager();
-                $image = $manager->make($file)->encode('svg');
-                $file_name = 'logo-transparent.' . $image->extension();
-                
                 // delete old logo
                 if ($site_settings->site_footer_logo) {
-                    unlink(storage_path('app/public/logo/' . $file_name));
+                    unlink(storage_path('app/public/logo/' . $site_settings->site_footer_logo));
                 }
 
-                $image->save(storage_path('app/public/logo/' . $file_name));
-                $site_settings->site_footer_logo = config('app.url') . '/storage/logo/' . $file_name;
+                $file_name = 'site_footer_logo' . $file->getClientOriginalExtension();
+                $file->storeAs('public/logo', $file_name);
+
+                $site_settings->site_footer_logo = $file_name;
             }
 
             if ($request->hasFile('site_favicon')) {
                 $file = $request->file('site_favicon');
 
-                // convert to svg
-                $manager = new ImageManager();
-                $image = $manager->make($file)->encode('svg');
-                $file_name = 'favicon.' . $image->extension();
-
-                // delete old logo
+                // delete old favicon
                 if ($site_settings->site_favicon) {
-                    unlink(storage_path('app/public/logo/' . $file_name));
+                    unlink(storage_path('app/public/logo/' . $site_settings->site_favicon));
                 }
 
-                $image->save(storage_path('app/public/logo/' . $file_name));
-                $site_settings->site_favicon = config('app.url') . '/storage/logo/' . $file_name;
+                $file_name = 'site_favicon' . $file->getClientOriginalExtension();
+                $file->storeAs('public/logo', $file_name);
+
+                $site_settings->site_favicon = $file_name;
             }
 
             $site_settings->site_name = $request->site_name;
